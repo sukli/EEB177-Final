0b1b2adb1f3f8c291037f2bd3c283259bb474a08
Suk Li <syli312@ucla.edu>
2015-03-18 03:06:28 -0700

    completed code

--- problem1.py
+++ problem1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import glob
+
 # concatenates all lines in the list into a single string
 def concat_list_lines(l):
     s = ""
@@ -18,7 +20,31 @@ def process_seq_file(filename):
     f.close()
     return (seq_name, seq, seq_len)
 
+# creates a log file with the appropriate header
+def create_log_file():
+    f = open("combined_seq.log", "w")
+    f.write("sequence_name,length\n")
+    return f
+
+def process_seq_files(filenames):
+    # open the output and log files for writing
+    output_file = open("combined.seq", "w")
+    log_file = create_log_file()
+    for filename in filenames:
+        # process file and write to output
+        (seq_name, seq, seq_len) = process_seq_file(filename)
+        output_file.write(">%s\n" % seq_name)
+        output_file.write("%s\n" % seq)
+
+        # record in log
+        log_file.write("%s,%d\n" % (seq_name, seq_len))
+
+    output_file.close()
+    log_file.close()
+
 def main():
+    seq_files = glob.glob("*.seq")
+    process_seq_files(seq_files)
 
 if __name__ == '__main__':
-    main()
+  main()
